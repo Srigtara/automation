d5f86a25c97e60464ac3ecc1213d23213eb2081b
deepa <a.deepashankar@gmail.com>
2020-11-27 03:01:50 -0800

    message

--- src/test/java/tests/TestApod.java
+++ src/test/java/tests/TestApod.java
@@ -1,39 +1,24 @@
 package tests;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import rest_assured.HttpClientHandler;
 import utils.CommonUtilities;
 import utils.Constants;
 import utils.ReadPropertyFile;
 
-public class TestUserDetails {
+public class TestApod {
 
 	ReadPropertyFile properties;
-	RequestSpecification httpRequest;
 	Response response;
 	public int statusCode;
-	public String json_Size;
-	public String statusLine;
-	public String contentType;
-	public String id;
-	public String title;
-	public String body;
 	ObjectMapper mapper;
 	JsonNode presentJson;
-	public String invalidUser;
-	Map<String, String> map = new HashMap<String, String>();
 
 	@BeforeTest(groups = { "smoke", "functionalTest" })
 	public void setUp() {
@@ -42,44 +27,14 @@ public class TestUserDetails {
 	}
 
 	@Test(groups = { "smoke" })
-	public void validate_APOD_API_is_Returning_Valid_Response() {
+	public void validate_APOD_API_is_Returning_200_Valid_Response() {
 		response = HttpClientHandler.requestGetCall("",
 				CommonUtilities.getQueryParams("API_KEY", "DATE", "HD1", "CONCEPT_TAG1"));
 		Assert.assertEquals(response.getStatusCode(), Constants.SUCCESS_CODE, "status code validation Failed");
 	}
 
 	@Test(groups = { "smoke" })
-	public void validate_APOD_API_is_Returning_Valid_Response1() {
-		String params = "";
-		Map<String, String> queryParamMap = new HashMap<String, String>();
-
-		queryParamMap.put("api_key", "FDoNxsFOpaowNUHWP6mLVWK9wW00b5se3a4ZOguC");
-		queryParamMap.put("date", "2020-11-26");
-		queryParamMap.put("hd", "true");
-		queryParamMap.put("concept_tags", "true");
-
-		response = HttpClientHandler.requestGetCall(params, queryParamMap);
-		statusCode = response.getStatusCode();
-		Assert.assertEquals(statusCode, Constants.SUCCESS_CODE, "status code validation Failed");
-	}
-
-	@Test(groups = { "smoke" })
-	public void validate_APOD_APIKEY_is_Returning_Valid_Response() {
-		String params = "";
-		Map<String, String> queryParamMap = new HashMap<String, String>();
-
-		queryParamMap.put("api_key", properties.getProperty("API_KEY"));
-		queryParamMap.put("date", "2020-11-26");
-		queryParamMap.put("hd", "true");
-		queryParamMap.put("concept_tags", "true");
-
-		response = HttpClientHandler.requestGetCall(params, queryParamMap);
-		statusCode = response.getStatusCode();
-		Assert.assertEquals(statusCode, Constants.SUCCESS_CODE, "status code validation Failed");
-	}
-
-	@Test(groups = { "smoke" })
-	public void validate_APOD_API_is_Returning_403InCase_of_Invalid_API_Key() {
+	public void validate_APOD_API_is_Returning_403_InCase_of_Invalid_API_Key() {
 		response = HttpClientHandler.requestGetCall("",
 				CommonUtilities.getQueryParams("INVALID_API_KEY", "DATE", "HD1", "CONCEPT_TAG1"));
 		statusCode = response.getStatusCode();
@@ -87,136 +42,50 @@ public class TestUserDetails {
 	}
 
 	@Test(groups = { "smoke" })
-	public void validate_APOD_API_is_Returning_404_With_Missing_Date_Value() {
-		String params = "";
-		Map<String, String> queryParamMap = new HashMap<String, String>();
-
-		queryParamMap.put("api_key", "FDoNxsFOpaowNUHWP6mLVWK9wW00b5se3a4");
-		queryParamMap.put("date", "");
-		queryParamMap.put("hd", "true");
-		queryParamMap.put("concept_tags", "true");
-
-		response = HttpClientHandler.requestGetCall(params, queryParamMap);
-		statusCode = response.getStatusCode();
-		Assert.assertEquals(statusCode, Constants.ERROR_FORBIDEN, "404 not returned");
-	}
-
-	@Test(groups = { "smoke" })
-	public void validate_APOD_API_is_Returning_400_With_FUTUREDATE() {
+	public void validate_APOD_API_is_Returning_400_InCase_of_Future_Date() {
 		response = HttpClientHandler.requestGetCall("",
 				CommonUtilities.getQueryParams("API_KEY", "FUTURE_DATE", "HD1", "CONCEPT_TAG1"));
-		statusCode = response.getStatusCode();
-		Assert.assertEquals(statusCode, Constants.ERROR_BADREQUEST, "400 not returned");
+		Assert.assertEquals(response.getStatusCode(), Constants.ERROR_BADREQUEST, "status code validation Failed");
 	}
 
 	@Test(groups = { "smoke" })
-	public void validate_APOD_API_is_Returning_403_With_DATEBEFORE_SERVICESTARTED() {
-		String params = "";
-		Map<String, String> queryParamMap = new HashMap<String, String>();
-
-		queryParamMap.put("api_key", "FDoNxsFOpaowNUHWP6mLVWK9wW00b5se3a4");
-		queryParamMap.put("date", "1995-06-15");
-		queryParamMap.put("hd", "true");
-		queryParamMap.put("concept_tags", "false");
-
-		response = HttpClientHandler.requestGetCall(params, queryParamMap);
-		statusCode = response.getStatusCode();
-		Assert.assertEquals(statusCode, Constants.ERROR_FORBIDEN, "403 not returned");
+	public void validate_APOD_API_is_Returning_400_InCase_of_Past_Date() {
+		response = HttpClientHandler.requestGetCall("",
+				CommonUtilities.getQueryParams("API_KEY", "PAST_DATE", "HD1", "CONCEPT_TAG1"));
+		Assert.assertEquals(response.getStatusCode(), Constants.ERROR_BADREQUEST, "status code validation Failed");
 	}
 
 	@Test(groups = { "smoke" })
-	public void validate_APOD_API_is_Returning_403_With_FUTUREDATE() {
-		String params = "";
-		Map<String, String> queryParamMap = new HashMap<String, String>();
-
-		queryParamMap.put("api_key", "FDoNxsFOpaowNUHWP6mLVWK9wW00b5se3a4");
-		queryParamMap.put("date", "2020-11-28");
-		queryParamMap.put("hd", "true");
-		queryParamMap.put("concept_tags", "false");
-
-		response = HttpClientHandler.requestGetCall(params, queryParamMap);
-		statusCode = response.getStatusCode();
-		Assert.assertEquals(statusCode, Constants.ERROR_FORBIDEN, "403 not returned");
+	public void validate_APOD_API_InCase_of_HD_False() {
+		response = HttpClientHandler.requestGetCall("",
+				CommonUtilities.getQueryParams("API_KEY", "DATE", "HD2", "CONCEPT_TAG1"));
+		Assert.assertEquals(response.getStatusCode(), Constants.SUCCESS_CODE, "status code validation Failed");
 	}
 
 	@Test(groups = { "smoke" })
+	public void validate_APOD_API_Is_returning_200_InCase_of_Concept_tag_False() {
+		response = HttpClientHandler.requestGetCall("",
+				CommonUtilities.getQueryParams("API_KEY", "DATE", "HD1", "CONCEPT_TAG2"));
+		Assert.assertEquals(response.getStatusCode(), Constants.SUCCESS_CODE, "status code validation Failed");
+	}
+
+	@Test(groups = { "smoke" })
+	public void validate_APOD_API_InCase_of_HD_Concept_Tag_False() {
+		response = HttpClientHandler.requestGetCall("",
+				CommonUtilities.getQueryParams("API_KEY", "DATE", "HD2", "CONCEPT_TAG2"));
+		Assert.assertEquals(response.getStatusCode(), Constants.SUCCESS_CODE, "status code validation Failed");
+	}
+
+	@Test(groups = { "smoke	" })
 	public void validate_APOD_API_is_Returning_403_With_EMPTYFIELDS() {
-		String params = "";
-		Map<String, String> queryParamMap = new HashMap<String, String>();
-
-		queryParamMap.put("api_key", " ");
-		queryParamMap.put("date", " ");
-		queryParamMap.put("hd", " ");
-		queryParamMap.put("concept_tags", " ");
-
-		response = HttpClientHandler.requestGetCall(params, queryParamMap);
-		statusCode = response.getStatusCode();
-		Assert.assertEquals(statusCode, Constants.ERROR_FORBIDEN, "403 not returned");
+		response = HttpClientHandler.requestGetCall("",
+				CommonUtilities.getQueryParams("NULL_API_KEY", "NULL_DATE", "NULL_HD", "NULL_CONCEPT_TAG"));
+		// statusCode = response.getStatusCode();
+		Assert.assertEquals(response.getStatusCode(), Constants.ERROR_FORBIDEN, "403 not returned");
 	}
-
-	/*
-	 * @Test(groups = { "smoke" }) public void
-	 * validate_New_User_creation_Functionality_Is_Working_Fine() {
-	 * map.put("userId", "2"); map.put("id", "19"); map.put("title",
-	 * "this is projectdebug.com"); map.put("body",
-	 * "this is REST-Assured Tutorial"); response =
-	 * HttpClientHandler.requestPostCall(map); statusCode =
-	 * response.getStatusCode(); CommonUtilities.flushMap(map);
-	 * Assert.assertEquals(statusCode, Constants.VALID_POST_REQUEST_CODE,
-	 * "status code validation failed"); }
-	 */
-
-	/*
-	 * @Test(groups = { "smoke" }) public void
-	 * validate_Delete_User_Functionality_Is_Working_Fine() { response =
-	 * HttpClientHandler.requestDeleteCall(properties.getProperty("VALID_ID"));
-	 * Assert.assertEquals(response.getStatusCode(), Constants.SUCCESS_CODE,
-	 * "status code validation failed"); }
-	 */
-//	@Test(groups = { "smoke" })
-//	public void validate_Status_Code_After_Updating_User_Details() throws FileNotFoundException {
-//		map.put("userId", properties.getpropertieserty("VALID_USER_ID"));
-//		map.put("id", properties.getpropertieserty("VALID_ID"));
-//		map.put("title", properties.getpropertieserty("UPDATED_TITLE"));
-//		map.put("body", properties.getpropertieserty("UPDATED_BODY"));
-//		response = HttpClientHandler.requestPutCall(properties.getpropertieserty("VALID_ID"), map);
-//		CommonUtilities.flushMap(map);
-//		Assert.assertEquals(response.getStatusCode(), Constants.SUCCESS_CODE, "status code validation failed");
-//
-//	}
-//
-//	@Test(groups = { "functionalTest" })
-//	public void validate_Id_Should_Not_Be_Updated() throws FileNotFoundException {
-//		map.put("userId", properties.getpropertieserty("VALID_USER_ID"));
-//		map.put("id", properties.getpropertieserty("UPDATED_ID"));
-//		map.put("title", properties.getpropertieserty("UPDATED_TITLE"));
-//		map.put("body", properties.getpropertieserty("UPDATED_BODY"));
-//		response = HttpClientHandler.requestPutCall(properties.getpropertieserty("VALID_ID"), map);
-//		JsonPath jsonPathEvaluator = response.jsonPath();
-//		id = jsonPathEvaluator.get("id").toString();
-//		CommonUtilities.flushMap(map);
-//		Assert.assertFalse(id == properties.getpropertieserty("UPDATED_ID"),
-//				"ID field got updated , api not working propertieserly");
-//
-//	}
-//
-//	@Test(groups = { "functionalTest" })
-//	public void validate_Response_Body_After_Performing_Update_Operation() throws FileNotFoundException {
-//		map.put("userId", properties.getpropertieserty("VALID_USER_ID"));
-//		map.put("id", properties.getpropertieserty("VALID_ID"));
-//		map.put("title", properties.getpropertieserty("UPDATED_TITLE"));
-//		map.put("body", properties.getpropertieserty("UPDATED_BODY"));
-//		response = HttpClientHandler.requestPutCall(properties.getpropertieserty("VALID_ID"), map);
-//		JsonPath jsonPathEvaluator = response.jsonPath();
-//		body = jsonPathEvaluator.get("body");
-//		CommonUtilities.flushMap(map);
-//		Assert.assertEquals(body, properties.getpropertieserty("UPDATED_BODY"), "body did not get updated");
-//
-//	}
 
 	@AfterTest(groups = { "smoke", "functionalTest" })
 	public void tearDown() {
-
 		CommonUtilities.endReport();
 	}
 
